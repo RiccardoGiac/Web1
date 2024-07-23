95f9210c1e4a8b4d8f5a82a97c5ba2c3cfa5c9b4
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-07-23 13:42:39 +0200

    fs

--- server/server.js
+++ server/server.js
@@ -15,8 +15,25 @@ console.log("Mi hai chiesto la form di registrazione");
 res.sendFile("formSemplice.html", { root: './htdoc' });
 });
 
+//pagina di gestione dei dati della form se il metodo è POST
+app.post('/mansendfile', (req, res) => {
+console.log(req.body.password)
+pass_ricevuta  = req.body.password;
+if(pass_ricevuta == "paperino")
+    res.send("<html>Bravo " + req.body.email + "<br>Sono pronto a ricevere il file!</html>")
+else
+    res.send("<html>Attenzione, password errata. </html>");
+
+});
+
+app.get('/sendFile', (req, res) => {
+    console.log("Mi hai chiesto la form di registrazione");
+    res.sendFile("sendfile.html", { root: './htdoc' });
+    });
+
 //pagina di gestione dei dati della form se il metodo è GET
 app.get('/gestisciDatiForm', (req, res) => {
 console.log(req.query.fname);
-res.send("<html>Buona serata a tutti</html>");
+console.log(req.query.fcognome);
+res.send("<html>Buona serata " + req.query.fname + " " + req.query.fcognome + "</html>");
 });
